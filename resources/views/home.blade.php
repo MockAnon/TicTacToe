00c2fba3e9c3b54ce0091bbc8600a0ff94f85d09
Jaron Evans <jarondevans@gmail.com>
2019-08-12 13:27:04 -0400

    added profile page with icon and started scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +12,11 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <div class="profile-picture">
+                        <img class="img-circle img-responsive" src="https://www.gravatar.com/avatar/{{ md5($user->email)}}?d=retro&s=200">
+                        <div> {{ $user->name }} </div>
+                        <div> {{ $user->score }}</div>
+                    </div>
                 </div>
             </div>
         </div>
