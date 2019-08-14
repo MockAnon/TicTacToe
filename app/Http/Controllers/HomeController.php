9f447d98057e4648747e14bca4c1170a535b8044
Jaron Evans <jarondevans@gmail.com>
2019-08-13 22:54:50 -0400

    worked on control

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
-        return view('home', compact('user'));
+        $users = User::paginate(5);
+        return view('home', compact('user', 'users'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,11 +14,25 @@
                     @endif
                     <div class="profile-picture">
                         <img class="img-circle img-responsive" src="https://www.gravatar.com/avatar/{{ md5($user->email)}}?d=retro&s=200">
-                        <div> {{ $user->name }} </div>
-                        <div> {{ $user->score }}</div>
+
+                    </div>
+
+                    <div class="profile-info">
+                        <div class="profile-username"> {{ $user->name }} </div>
+                        <div class="profile-score"> {{ $user->score }}</div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="col-md-8">
+            <div class="panel panel-default"> 
+                <div class="panel-body">
+                    <div class="list-group">
+                        @foreach($users as $_user)
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
